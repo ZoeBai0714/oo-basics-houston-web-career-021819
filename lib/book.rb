590431065f1c792bfbd7f33d573965d3057892f0
Zoe Bai <s.xue.bai@gmail.com>
2019-02-21 04:43:38 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,9 @@
 class Book
   def initialize(title)
     @title = title
-    @author =author
-    @pages =pages
-    @genre =genre
+    @author = author
+    @pages = pages
+    @genre = genre
   end
 end
 
